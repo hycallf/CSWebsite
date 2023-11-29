e473aebf67cdf1dfa9969a39d95c8b8612a128bb
Muhammad Haikal Fuady <ricardohaikal2001@gmail.com>
2023-11-29 09:47:11 +0700

    publikasi

--- app/Http/Controllers/PublikasiController.php
+++ app/Http/Controllers/PublikasiController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Publication;
+use App\Models\Mahasiswa;
+use App\Models\Dosen;
 
 class PublikasiController extends Controller
 {
